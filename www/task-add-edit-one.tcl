542e5944fbb137017c40e8c291ec1002af59ccf2
Malte Sussdorff <openacs@sussdorff.de>
2005-06-18 07:10:42 +0000

    Removed testing code

--- www/task-add-edit-one.tcl
+++ www/task-add-edit-one.tcl
@@ -29,8 +29,6 @@ ad_page_contract {
     {process_task_id:integer,multiple ""}
     {return_url ""}
     {assignee:array,multiple,optional}
-    {organization_id "1302"}
-    {customer_id "1302"}
 }
 
 
