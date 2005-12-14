8d3c7695be9e2f370aa2696fd6975eaac45ca7b4
Malte Sussdorff <openacs@sussdorff.de>
2005-12-14 17:50:23 +0000

    Support searching contacts if contacts is installed

--- lib/search-jobid.tcl
+++ lib/search-jobid.tcl
@@ -29,21 +29,25 @@ ad_form -name search_jobid -form {
     }
     {return_url:text(hidden) {value $return_url}}
 } -on_submit {
-    set match_projects [db_list_of_lists get_projects { }]
-    set match_length [llength $match_projects]
-    if { [string equal $match_length 0] } {
-	# No Match just redirect
-	ad_returnredirect $return_url
+    if {[string is integer $keyword]} {
+	set match_projects [db_list_of_lists get_projects { }]
+	set match_length [llength $match_projects]
+	if { [string equal $match_length 0] } {
+	    # No Match just redirect
+	    ad_returnredirect $return_url
+	} else {
+	    set project_item_id [lindex [lindex $match_projects 0] 0]
+	    set object_package_id [lindex [lindex $match_projects 0] 2]
+	    
+	    # We get the node_id from the package_id and use it 
+	    # to get the url of the project-manager
+	    set pm_node_id [site_node::get_node_id_from_object_id -object_id $object_package_id]
+	    set pm_url [site_node::get_url -node_id $pm_node_id]
+	    
+	    # Just redirect to the pm_url and project_item_id
+	    ad_returnredirect "${pm_url}one?project_item_id=$project_item_id"
+	}
     } else {
-	set project_item_id [lindex [lindex $match_projects 0] 0]
-	set object_package_id [lindex [lindex $match_projects 0] 2]
-
-	# We get the node_id from the package_id and use it 
-	# to get the url of the project-manager
-	set pm_node_id [site_node::get_node_id_from_object_id -object_id $object_package_id]
-       	set pm_url [site_node::get_url -node_id $pm_node_id]
-	
-	# Just redirect to the pm_url and project_item_id
-	ad_returnredirect "${pm_url}one?project_item_id=$project_item_id"
+	ad_returnredirect [export_vars -base "/contacts/" -url {{query $keyword}}]
     }
 }
